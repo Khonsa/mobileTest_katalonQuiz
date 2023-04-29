e9d6b38b666e73d961bf10901f65b1dac081a9a3
Khonsa <khonsaazizaalmash@gmail.com>
2023-04-30 00:33:09 +0700

    add tc negative case

---
+++ Scripts/SK01-Login/Positive Case/TC002-Login_Registered_User/Script1682788443892.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('E:\\Android-MyDemoAppRN.1.2.0.build-231_2.apk', true)
+
+Mobile.tap(findTestObject('P01-Product/btn-burgerMenu'), 0)
+
+Mobile.scrollToText('Log In')
+
+Mobile.tap(findTestObject('P01-Product/btn-BurgerMenu_login'), 0)
+
+Mobile.setText(findTestObject('P02-Login/editText-usernameLogin'), 'bob@example.com', 0)
+
+Mobile.setEncryptedText(findTestObject('P02-Login/editText-passwordLogin'), 'dbXIBZ66cuht4KTRMDozZw==', 0)
+
+Mobile.tap(findTestObject('P02-Login/btn-login'), 0)
+
+Mobile.verifyElementText(findTestObject('P01-Product/textView-titleProductPage'), 'Products')
+
+Mobile.closeApplication()
+
